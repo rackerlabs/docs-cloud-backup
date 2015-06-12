59f8ed7e79581c590402beaca38e7b7958ba88a4
Zachary Corleissen <zachary.corleissen@gmail.com>
2015-06-12 17:25:32 -0500

    Adding standard conf.py

--- rst/devguide/cloud-backup-v2/conf.py
+++ rst/devguide/cloud-backup-v2/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Rackspace Developer Portal documentation build configuration file, created by
-# sphinx-quickstart on Fri Jun 12 16:51:53 2015.
+# sphinx-quickstart on Fri Jun 12 16:36:52 2015.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -108,7 +108,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
